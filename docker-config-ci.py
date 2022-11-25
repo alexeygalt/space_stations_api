5f34c895003c031bdf880e15ef1d998c0eb59cb4
alexeygalt <alexeygalt@gmail.com>
2022-11-25 16:25:59 +0300

    deploy add

---
+++ docker-config-ci.py
@@ -0,0 +1,5 @@
+class Config(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://$DB_USER:$DB_PASSWORD@pg/$DB_NAME'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    RESTX_JSON = {'ensure_ascii': False, 'indent': 2}
